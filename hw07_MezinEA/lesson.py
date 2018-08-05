c0f5709c1cb896913ea5c107ba0725aaff90f1a5
EA <ea.mezin@gmail.com>
2018-08-06 00:20:46 +0500

    lesson7

--- hw07_MezinEA/lesson.py
+++ hw07_MezinEA/lesson.py
@@ -1,9 +1,19 @@
-v1 = [1, 2, 3]
-v2 = [3, 5, 7]
-
-print(list(sum(zip(*v1, *v2))))
+import math
 
 
-map()
-filter()
-zip()
+a = 4.4
+b = 4.5
+e = 0.00001
+n = 0
+
+while e < abs(a-b):
+    c = (a + b)/2
+    if math.tan(c) == c:
+        break
+    elif math.tan(a) * math.tan(c) > c*c:
+        b = c
+    else:
+        a = c
+    n = n + 1
+    print(f'{n} - a = {a}, b = {b}, c = {c}')
+
